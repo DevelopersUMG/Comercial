71a57c54d6c337509db602843a2fc9c0c5a0cb56
Luis Ramos <guicholarg@hotmail.com>
2013-11-06 18:42:58 -0600

    Módulo de facturación, versión 1
    El módulo de facturación incluye el ingreso y la consulta de facturas

---
+++ Luis Ramos/Transaccional/Transaccional/Detalle.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Transaccional
+{
+    partial class Detalle
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(955, 315);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Detalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Detalle";
+            this.Size = new System.Drawing.Size(986, 465);
+            this.Load += new System.EventHandler(this.Detalle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Luis Ramos/Transaccional/Transaccional/Detalle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ODBCConnect;
+
+namespace Transaccional
+{
+    public partial class Detalle : UserControl
+    {
+
+        int factura, serie, bodega;
+
+        public Detalle(int f, int s, int b)
+        {
+            InitializeComponent();
+            this.factura = f;
+            this.serie = s;
+            this.bodega = b;
+        }
+
+        private void Detalle_Load(object sender, EventArgs e)
+        {
+            string query = "select p.nombre as 'Producto', p.precio as 'Precio', d.cantidad as 'Cantidad' ";
+            query += "from detalle_factura d, producto p where d.idproducto=p.idproducto and d.idbodega=" + bodega;
+            query += " and d.no_factura=" + factura + " and d.serie=" + serie;
+            dataGridView1.DataSource = new DBConnect("factura").consulta_DataGridView(query);
+        }
+    }
+}

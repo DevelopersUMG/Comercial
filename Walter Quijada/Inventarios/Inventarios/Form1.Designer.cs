00a24404b7391c5f2d6d6b646760902833b82be8
walterqf <walterqfolgar@hotmail.com>
2013-11-09 08:58:17 -0600

    Modulo de Inventarios
    Versión para unión al principal

---
+++ Walter Quijada/Inventarios/Inventarios/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Inventarios
+{
+    partial class fr_p
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ms_p = new System.Windows.Forms.MenuStrip();
+            this.consultarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bodegaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ms_p.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ms_p
+            // 
+            this.ms_p.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultarToolStripMenuItem});
+            this.ms_p.Location = new System.Drawing.Point(0, 0);
+            this.ms_p.Name = "ms_p";
+            this.ms_p.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.ms_p.Size = new System.Drawing.Size(913, 24);
+            this.ms_p.TabIndex = 1;
+            this.ms_p.Text = "menuStrip1";
+            // 
+            // consultarToolStripMenuItem
+            // 
+            this.consultarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bodegaToolStripMenuItem,
+            this.consultasToolStripMenuItem});
+            this.consultarToolStripMenuItem.Image = global::Inventarios.Properties.Resources.índice1;
+            this.consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            this.consultarToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.consultarToolStripMenuItem.Text = "Consultar";
+            // 
+            // bodegaToolStripMenuItem
+            // 
+            this.bodegaToolStripMenuItem.Name = "bodegaToolStripMenuItem";
+            this.bodegaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.bodegaToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.bodegaToolStripMenuItem.Text = "Bodega (Tabla)";
+            this.bodegaToolStripMenuItem.Click += new System.EventHandler(this.bodegaToolStripMenuItem_Click);
+            // 
+            // consultasToolStripMenuItem
+            // 
+            this.consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            this.consultasToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.consultasToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.consultasToolStripMenuItem.Text = "Existencias";
+            this.consultasToolStripMenuItem.Click += new System.EventHandler(this.consultasToolStripMenuItem_Click);
+            // 
+            // fr_p
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 616);
+            this.Controls.Add(this.ms_p);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.ms_p;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "fr_p";
+            this.Text = "Control Principal";
+            this.Load += new System.EventHandler(this.fr_p_Load);
+            this.ms_p.ResumeLayout(false);
+            this.ms_p.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip ms_p;
+        private System.Windows.Forms.ToolStripMenuItem consultarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bodegaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem;
+
+    }
+}
+
---
+++ Walter Quijada/Inventarios/Inventarios/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Inventarios
+{
+    public partial class fr_p : Form
+    {
+        public fr_p()
+        {
+            InitializeComponent();
+        }
+
+        private void bodegaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fr_bodega n = new fr_bodega();
+            n.MdiParent = this;
+            n.WindowState = FormWindowState.Maximized;
+            n.Show();
+        }
+
+        private void consultasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fr_consultas n = new fr_consultas();
+            n.MdiParent = this;
+            n.WindowState = FormWindowState.Maximized;
+            n.Show();
+        }
+
+        private void fr_p_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
